bb9c49e5672397bd88d93456d2ef194ee1bdd19c
IT-DM <garifulin.1309@gmail.com>
2021-04-17 16:42:18 +0700

    Добавьте файлы проекта.

---
+++ Web/Controllers/HomeController.vb
@@ -0,0 +1,19 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Тест"
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+End Class
---
+++ Web/Views/Home/About.vbhtml
@@ -0,0 +1,11 @@
+﻿@Code
+    ViewData("Title") = "Привет"
+End Code
+
+<h2>@ViewData("Title").</h2>
+
+    <img src ="https://i.gifer.com/WS56.gif ">
+
+<h3>@ViewData("Message")</h3>
+
+<p>Ненужная бесполезная информация</p>
